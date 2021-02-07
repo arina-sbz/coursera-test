d3dc0e89a2d017b2e0bfcd94376dfe4595d448ed
arina-sbz <71599664+arina-sbz@users.noreply.github.com>
2021-02-07 19:44:11 +0330

    Add files via upload

---
+++ module4/SpeakGoodBye.js
@@ -0,0 +1,10 @@
+
+(function(window){
+	var byeSpeaker ={};
+	var speakWord = "Good Bye";
+	byeSpeaker.speak= function(){
+		console.log(speakWord+" " + names[i]);
+	}
+
+	window.byeSpeaker = byeSpeaker;
+})(window);
